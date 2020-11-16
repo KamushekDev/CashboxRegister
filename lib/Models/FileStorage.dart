36e27b0b7f83644972efed94356eb40aaa190a9f
KamushekDev <kamushek.stone@gmail.com>
2020-11-16 20:44:47 +0300

    Removed comment

--- lib/Models/FileStorage.dart
+++ lib/Models/FileStorage.dart
@@ -34,26 +34,3 @@ class FileStorage {
     }
   }
 }
-
-// class CounterStorage {
-//   Future<int> readCounter() async {
-//     try {
-//       final file = await _localFile;
-//
-//       // Read the file
-//       String contents = await file.readAsString();
-//
-//       return int.parse(contents);
-//     } catch (e) {
-//       // If encountering an error, return 0
-//       return 0;
-//     }
-//   }
-//
-//   Future<File> writeCounter(int counter) async {
-//     final file = await _localFile;
-//
-//     // Write the file
-//     return file.writeAsString('$counter');
-//   }
-// }
